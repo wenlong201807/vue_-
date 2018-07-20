f7133b8c1da5a190c89e3b0b45151800ba1b6136
kreviazuk <kreviazukzhou@gmail.com>
2018-07-20 21:05:59 +0800

    add webpack demos

---
+++ kreviazukzhou/webpack/src/index.js
@@ -0,0 +1,20 @@
+import print from './print';
+import { cube } from './math.js';
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+}
+
+function component() {
+    var element = document.createElement("pre");
+    element.innerHTML = [
+        'Hello webpack!',
+        '5 cubed is equal to ' + cube(5)
+    ].join('\n\n');
+}
+document.body.appendChild((component()));
+if (module.hot) {
+    module.hot.accept('./print.js', function() {
+        console.log('Accepting the updated printMe module!');
+        printMe();
+    })
+}
